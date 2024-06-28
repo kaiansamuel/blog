a38a3ef77d9fdf485247295b5480a8eaa94de734
Kaian Samuel <kaiansamuelster@gmail.com>
2024-06-28 14:55:09 -0300

    Pagina de Cadastro

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,6 +17,18 @@ const Navbar = () => {
           </NavLink>
         </li>
         <li>
+          <NavLink to='/login' className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Entrar
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/register' className={({ isActive }) => (isActive ? styles.active : "")}
+          >
+            Cadastrar
+          </NavLink>
+        </li>
+        <li>
           <NavLink to='/about' className={({ isActive }) => (isActive ? styles.active : "")}
           >
             Sobre
